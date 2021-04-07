3ceecbcfc169dc24d84b4546242965026b0d881b
zhangzuoting <18844144647@163.com>
2021-04-07 22:26:56 +0800

    第一次修改

--- zentao/module/job/control.php
+++ zentao/module/job/control.php
@@ -242,7 +242,7 @@ class job extends control
     {
         $status = $this->job->exec($id);
         if(dao::isError()) die(js::error(dao::getError()));
-//注解
+//提交1
         $this->app->loadLang('compile');
         echo js::alert(sprintf($this->lang->job->sendExec, zget($this->lang->compile->statusList, $status)));
         die(js::reload('parent'));
